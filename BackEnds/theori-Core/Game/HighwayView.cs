38c92bfa6905b3e934a2b507baeb7e503f492b59
Local Atticus <local.atticus@gmail.com>
2019-05-24 01:09:08 -0400

    Fix camera far clipping plane

--- BackEnds/theori-Core/Game/HighwayView.cs
+++ BackEnds/theori-Core/Game/HighwayView.cs
@@ -279,7 +279,7 @@ namespace theori.Game
             HorizonHeight = (0.5f + (pitchDeg / Camera.FieldOfView)) * Camera.ViewportHeight;
 
             Camera.Position = CameraOffset;
-            Camera.FarDistance = Vector3.Transform(new Vector3(0, 0, LENGTH_BASE), WorldTransform.Matrix).Length();
+            Camera.FarDistance = Vector3.Transform(new Vector3(0, 0, -LENGTH_BASE), WorldTransform.Matrix).Length();
         }
 
         public void Render()
