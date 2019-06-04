f06ed1731baf2faff1d085e9d487a8e195e8c04a
Local Atticus <local.atticus@gmail.com>
2019-06-04 01:11:39 -0400

    Create a simple menu navigation for the startup screen

--- GameModes/NeuroSonic/Plugin.cs
+++ GameModes/NeuroSonic/Plugin.cs
@@ -1,18 +1,24 @@
 ﻿using theori;
 
 using NeuroSonic.GamePlay;
+using NeuroSonic.Startup;
 
 namespace NeuroSonic
 {
     internal static class Plugin
     {
+        public static string[] ProgramArgs { get; private set; }
+
         /// <summary>
         /// Invoked when the plugin starts in Standalone.
         /// </summary>
         public static void NSC_Main(string[] args)
         {
+            ProgramArgs = args;
+
             // TODO(local): push the game loading layer, which creates the game layer
-            Host.PushLayer(new GameLayer(true));
+            //Host.PushLayer(new GameLayer(true));
+            Host.PushLayer(new NeuroSonicStandaloneStartup());
         }
     }
 }
--- music-theori/BootLoaders/StandaloneBootLoader.cs
+++ music-theori/BootLoaders/StandaloneBootLoader.cs
@@ -1,13 +1,30 @@
-﻿namespace theori.BootLoaders
+﻿using System.Numerics;
+
+using theori.Graphics;
+using theori.Gui;
+
+namespace theori.BootLoaders
 {
     public sealed class StandaloneBootLoader : Layer
     {
+        private Panel m_guiRoot;
+
         public StandaloneBootLoader(string[] args)
         {
         }
 
         public override void Init()
         {
+            m_guiRoot = new Panel()
+            {
+                Children = new GuiElement[]
+                {
+                    new TextLabel(Font.Default24, "music:theori Standalone Boot Loader")
+                    {
+                        Position = new Vector2(20, 20),
+                    },
+                }
+            };
         }
 
         public override void Destroy()
@@ -28,6 +45,25 @@
 
         public override void Render()
         {
+            void DrawUiRoot(Panel root)
+            {
+                if (root == null) return;
+
+                var viewportSize = new Vector2(Window.Width, Window.Height);
+                using (var grq = new GuiRenderQueue(viewportSize))
+                {
+                    root.Position = Vector2.Zero;
+                    root.RelativeSizeAxes = Axes.None;
+                    root.Size = viewportSize;
+                    root.Rotation = 0;
+                    root.Scale = Vector2.One;
+                    root.Origin = Vector2.Zero;
+
+                    root.Render(grq);
+                }
+            }
+
+            DrawUiRoot(m_guiRoot);
         }
     }
 }
